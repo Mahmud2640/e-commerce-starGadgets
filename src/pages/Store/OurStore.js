b9eee0bbd9ae8dc8ab516d9480d8e0c9138e4949
Mahmud2640 <mdmahmud484@gmail.com>
2023-02-20 09:07:35 +0600

    our store

--- src/pages/Store/OurStore.js
+++ src/pages/Store/OurStore.js
@@ -8,8 +8,84 @@ const OurStore = () => {
     <>
       <Meta title={"Our Store"} />
       <BreadCrumb title="Our Store" />
+      <div className="store-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Shop By Categories</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li>Watch</li>
+                    <li>Tv</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Filter By</h3>
+                <div>
+                  <h5 className="sub-title">Availablity</h5>
+                  <div>
+                    <div classname="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        In Stock (1)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        Out of Stock (0)
+                      </label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Price</h5>
+                  <div className="d-flex align-items-center gap-10">
+                    <div className="form-floating mb-3">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="floatingInput"
+                        placeholder="Name"
+                      />
+                      <label htmlFor="floatingInput">From</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="floatingInput"
+                        placeholder="Name"
+                      />
+                      <label htmlFor="floatingInput">To</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Product Tags</h3>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Random Product</h3>
+              </div>
+            </div>
+            <div className="col-9"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
-
 export default OurStore;
